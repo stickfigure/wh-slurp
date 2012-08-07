2c4c3dd4755a569aa4bc603933734e000ef8df40
Jeff Schnitzer <jeff@infohazard.org>
2012-08-07 13:35:42 -0700

    Fixed java startup script

--- main-java.sh
+++ main-java.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-java -cp java/lib/jackson-annotations-2.0.2.jar:java/lib/jackson-databind-2.0.2.jar:java/lib/jackson-core-2.0.jar:java/lib/simple-4.1.21.jar:build/classes Slurp
+java -cp java/lib/jackson-annotations-2.0.2.jar:java/lib/jackson-databind-2.0.2.jar:java/lib/jackson-core-2.0.2.jar:java/lib/simple-4.1.21.jar:build/classes Slurp
