1cc67577973ae06c3d42dff188c55a135daa63a9
Jeff Schnitzer <jeff@infohazard.org>
2012-08-18 17:32:40 -0400

    New urls for slurp

--- runab.sh
+++ runab.sh
@@ -1,1 +1,1 @@
-ab -c $1 -n $2 -r "$3/receive?n=user_name&c=country&t=tagline&s=123&b=best_word&u=8&bn=55&i=input_stream&r=23"
+ab -c $1 -n $2 -r "$3/slurp/receive?n=user_name&c=country&t=tagline&s=123&b=best_word&u=8&bn=55&i=input_stream&r=23"
