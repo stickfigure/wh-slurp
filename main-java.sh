a2c04cb269e6d64d482836dfb7b41098303753fd
Jeff Schnitzer <jeff@infohazard.org>
2012-08-24 00:30:08 -0400

    Removed experiments now that they have been tagged

--- main-java.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-java -Xmx128M -cp java/lib/jackson-annotations-2.0.2.jar:java/lib/jackson-databind-2.0.2.jar:java/lib/jackson-core-2.0.2.jar:java/lib/simple-4.1.21.jar:build/classes Slurp
